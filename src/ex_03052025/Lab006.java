5373ec3d41f88129e815c6c22ca87bb877c59a03
Rishabh.Mishra <rsb810@gmail.com>
2025-07-25 13:49:54 +0530

    nothing much

--- src/ex_03052025/Lab006.java
+++ src/ex_03052025/Lab006.java
@@ -22,5 +22,7 @@ public class Lab006 {
         float avg  = (marks[0]+marks[1]+marks[2]+marks[3]+marks[4])/5;
 
         System.out.println("your avg is-->  "+avg);
+        System.out.println("your avg is-->  "+avg);
+
     }
 }
